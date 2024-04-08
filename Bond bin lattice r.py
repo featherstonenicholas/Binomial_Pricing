e553dbd02e28f08d953bd54a16affd2141e8e0a7
Nicholas Featherstone <featherstonenicholas@gmail.com>
2024-04-08 10:23:36 +0100

    added hazard rates and defaultable option to bond calculations

--- Bond bin lattice r.py
+++ Bond bin lattice r.py
@@ -1,36 +1,48 @@
 import numpy as np
 
 #initialise parameters
-r0=0.06
-u=1.25
+r0=0.05
+u=1.1
 d=0.9
-T=6
+T=10
 q=0.5
+F=100 #face value
+c=0.0 #coupon rate
+R=0.2 #recovery rate
+a=0.01 #parameters for hazard rates
+b=1.01
 
 #create rate lattice calculation
 def rate(r0,u,d,T):
   C = r0 * d ** (np.arange(T,-1,-1)) * u ** (np.arange(0,T+1,1)) 
   return C
+#add in hazard rate calculations. For non defaultable, set a=0
+
+def hazard(a,b,T):
+    h=a*b**(np.arange(0,T+1,1)-T/2)
+    return h
 
 #pricing a bond following lattice term structure for risk free rate
 
-F=100 #face value
-c=0.1 #coupon rate
-def bond(F,r0,q,u,d,t,T,c):
+
+def bond(F,r0,q,u,d,a,b,R,t,T,c):
     c=F*c # initialise coupon payment value
+    rec=R*F
     #initialise final payment array
     F=(F+c)*np.ones(T+1)
     
     for i in np.arange(T,t,-1):
         disc= 1/(1+rate(r0,u,d,i-1))
-        F = disc * ( q * F[1:i+1] + (1-q) * F[0:i] )
+        F = disc * ( (1-hazard(a,b,i-1)) *(q* F[1:i+1] + (1-q) * F[0:i] )+hazard(a,b,i-1)*rec)
         F+=c*np.ones(i)
         
     return F
 
+
+
 #find forward price of a coupon bearing bond
 
-Z_4=bond(F,r0,q,u,d,4,T,c)-(F*c)*np.ones(len(bond(F,r0,q,u,d,4,T,c))) #value of a 2 year bond at t=4
+Z_4=bond(F,r0,q,u,d,a,b,R,4,T,c)-(F*c)*np.ones(len(bond(F,r0,q,u,d,a,b,R,4,T,c))) #value of a 2 year bond at t=4
 
 #define a discounted expectation of an array
 def disc_exp(F,r0,q,u,d):
@@ -43,7 +55,7 @@ def disc_exp(F,r0,q,u,d):
         
     return F
 #forward price of a 2 year coupon paying bond at t=4
-forward=100*disc_exp(Z_4,r0,q,u,d)/bond(F,r0,q,u,d,0,4,0)
+forward=100*disc_exp(Z_4,r0,q,u,d)/bond(F,r0,q,u,d,a,b,R,0,4,0)
 print(forward)
 
 #define non discounted exp
